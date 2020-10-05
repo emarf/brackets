fb4ee0b15d79c369fa7e883f88a73d499bf36b51
emarf <frame.toravich@gmail.com>
2020-10-05 19:55:00 +0300

    feat: implement brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let joinBracketsConfig = bracketsConfig.join(' ').replace(/,/g, '').split(' ').reverse();
+    joinBracketsConfig.map(item => { // go through each element of the array(use like counter)
+        for (let i = 0; i < joinBracketsConfig.length; i++) {
+            while (str.includes(joinBracketsConfig[i])) {
+                str = str.replace(joinBracketsConfig[i], '');
+                console.log(str);
+            }
+        }
+    })
+
+    return (str === '') ? true : false;
 }
